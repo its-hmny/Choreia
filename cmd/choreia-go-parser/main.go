fb0564a4012bbe7d27fa2072a6d2a189b981e079
Enea Guidi <guidienea@gmail.com>
2021-09-06 23:15:08 +0200

    Added WIP Assignment statement parsing function

--- cmd/choreia-go-parser/main.go
+++ cmd/choreia-go-parser/main.go
@@ -1,35 +1,67 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"log"
-	"os"
 )
 
+func ParseAssignment(stmt *ast.AssignStmt) (bool, error) {
+	if len(stmt.Lhs) != len(stmt.Rhs) {
+		return false, errors.New("should receive same number of l_val and r_val")
+	}
+
+	for i, _ := range stmt.Lhs {
+		lVal, rVal := stmt.Lhs[i], stmt.Rhs[i]
+		tmp, ok := rVal.(*ast.CallExpr)
+
+		// TODO ADD FUNCTIONALITIES
+		// Check that the function returned is a "make"
+		// Determine if it's a channel (bounded or unbounded)
+		// Extrapolate the type of the channel
+		if ok {
+			fmt.Printf("%s %s \t", lVal, tmp.Args[0])
+		}
+
+		// TODO debug pourposes, can remove
+		if stmt.Tok == token.DEFINE {
+			fmt.Printf("DEFINE %s %T \n", lVal, stmt.Rhs[i])
+		} else if stmt.Tok == token.ASSIGN {
+			fmt.Printf("ASSIGN %s %T \n", lVal, stmt.Rhs[i])
+		}
+	}
+	return true, nil
+}
+
 func inspector(node ast.Node) bool {
-	switch nodetype := node.(type) {
-	case *ast.ArrayType:
-		fmt.Println()
+	switch typedNode := node.(type) {
+	// In case of an assignment we're interested in extrapolating
+	// the infos about eventual channel declaration
 	case *ast.AssignStmt:
-		fmt.Println()
-	case *ast.BasicLit:
-		fmt.Println()
-	case *ast.BinaryExpr:
-		fmt.Println()
-	case *ast.BlockStmt:
-		fmt.Println()
-	case *ast.BranchStmt:
-		fmt.Println()
-	case *ast.CallExpr:
-		fmt.Println()
-	case *ast.CaseClause:
-		fmt.Println()
-	// TODO ADD MISSING ONES
-	default:
-		fmt.Println(nodetype)
+		_, err := ParseAssignment(typedNode)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	// Error handling case
+	case *ast.BadDecl, *ast.BadExpr, *ast.BadStmt:
+		log.Fatalf("Error at line %d, column %d", node.Pos(), node.End())
+		//case *ast.BasicLit:
+		//	fmt.Println()
+		//case *ast.BinaryExpr:
+		//	fmt.Println()
+		//case *ast.BlockStmt:
+		//	fmt.Println()
+		//case *ast.BranchStmt:
+		//	fmt.Println()
+		//case *ast.CallExpr:
+		//	fmt.Println()
+		//case *ast.CaseClause:
+		//	fmt.Println()
+		// TODO ADD MISSING ONES
 	}
 	return true
 }
@@ -39,15 +71,15 @@ func main() {
 	log.SetPrefix("[choreia-go-parser]: ")
 	log.SetFlags(0)
 
-	if len(os.Args) < 2 {
-		log.Fatal("A path to an existing go source file is neeeded")
-	}
+	//if len(os.Args) < 2 {
+	//	log.Fatal("A path to an existing go source file is neeeded")
+	//}
 
 	// Positions are relative to fset
 	fset := token.NewFileSet()
 	flags := parser.Trace | parser.DeclarationErrors | parser.AllErrors
 	// Parse the file identified by the given path and print the tree to the terminal.
-	f, err := parser.ParseFile(fset, os.Args[1], nil, flags)
+	f, err := parser.ParseFile(fset, "test/basic/channel.go", nil, flags)
 
 	if err != nil {
 		fmt.Println(err)
--- test/basic/channel.go
+++ test/basic/channel.go
@@ -11,20 +11,25 @@ v := <-ch  // Receive from ch, and assign value to v.
 */
 
 func sum(s []int, c chan int) {
-	sum := 0
+	accumulator := 0
 	for _, v := range s {
-		sum += v
+		accumulator += v
 	}
-	c <- sum // send sum to c
+	// Send accumulator to c
+	c <- accumulator
 }
 
 func main() {
 	s := []int{7, 2, 8, -9, 4, 0}
 
-	c := make(chan int)
+	var c chan int
+	c = make(chan int)
+
 	go sum(s[:len(s)/2], c)
 	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c // receive from c
+	x, y := <-c, <-c // Receive from c
+
+	close(c)
 
 	fmt.Println(x, y, x+y)
 }
