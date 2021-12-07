96961e85a0ba4be612960ee22dbc7a1d50d388fa
Enea <guidienea@gmail.com>
2021-12-07 19:21:13 +0100

    refactor(fsa): Complete refactor of FSA implementation

--- internal/data_structures/fsa/fsa.go
+++ internal/data_structures/fsa/fsa.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"log"
 
+	list "github.com/emirpasic/gods/lists/singlylinkedlist"
 	"github.com/goccy/go-graphviz"
 	"github.com/goccy/go-graphviz/cgraph"
 )
@@ -22,189 +23,164 @@ const (
 	Unknown = -1
 )
 
-// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
 // FSA
 
 // A FSA is a graph that represents all the possible transition from a state to the others
 //
-// A struct containing a basic graph implementation that keeps track of the transaction that occur
-// subsequently during the execution flow of a function (or scope).
+// A struct containing a basic graph implementation that keeps track of the transition that
+// occurs subsequently during the execution flow of a function (or scope).
 type FSA struct {
-	currentId int     // The last id generated, the id of the last node
-	states    []State // The list of state inside the graph
+	currentId   int                          // The last id generated, the id of the last node
+	transitions map[int]map[int][]Transition // Adjacency matrix of transition from edge to edge
+	FinalStates *list.List                   // A list containing the ids of the final/accepting states
 }
 
-type State struct {
-	Id         int                  // The id of the current state
-	IsFinal    bool                 // Flag to indicate that a state si final/accepting
-	transition map[int][]Transition // A map to other stateId the Transition data
-}
-
-// This function generates a new FSA and returns a pointer reference to it
+// Generates a new empty FSA and returns a pointer reference to it
 func New() *FSA {
-	return &FSA{
-		currentId: 0,
-		// Every FSA has already the first (0) state inside
-		states: []State{{Id: 0, IsFinal: false, transition: make(map[int][]Transition)}},
+	newFsa := FSA{
+		currentId:   0,
+		FinalStates: list.New(),
+		transitions: make(map[int]map[int][]Transition),
 	}
+
+	return &newFsa
 }
 
 // This function generates an independent copy of the given FSA and returns it
 func (original *FSA) Copy() *FSA {
-	fsaCopy := FSA{
+	localCopy := FSA{
 		currentId: original.currentId,
-		states:    make([]State, 0, len(original.states)),
+		// TODO DOES IT COPIES
+		FinalStates: original.FinalStates, // Dereferences the list forcing a copy on the object
+		transitions: make(map[int]map[int][]Transition),
 	}
 
-	for _, currentState := range original.states {
-		// Creates a new map, overriding the copied one (since map instance remains intertwined)
-		currentState.transition = make(map[int][]Transition)
-		// Copies all the entry of the other map into the decoupled one
-		for dest, destTransitions := range original.states[currentState.Id].transition {
-			tCopy := make([]Transition, len(destTransitions))
-			copy(tCopy, destTransitions)
-			currentState.transition[dest] = tCopy
-		}
-
-		// Then adds the fully copied state to the copy FSA
-		fsaCopy.states = append(fsaCopy.states, currentState)
-	}
-
-	return &fsaCopy
-}
-
-// Returns the latest generated id
-func (fsa *FSA) GetLastId() int {
-	return len(fsa.states) - 1
-}
-
-// Set a new rootId, a rootId is the id of the State (state) from which all future transition
-// will start when an id isn't specified, this is used since when merging multiple subgraph is
-// needed that the merge state will become the one from which create new transition even if it
-// is not the last created state
-func (fsa *FSA) SetRootId(newRootId int) {
-	fsa.currentId = newRootId
-}
-
-// Returns the id of the final state, if such state is not present returns Unknown
-func (fsa *FSA) GetFinalStateId() int {
-	return 0 // TODO REDO COMPLETELY
-}
-
-// Returns the state identified by the given id,if not found returns error
-func (fsa *FSA) GetState(stateId int) State {
-	found := fsa.states[stateId]
-	return found
-}
-
-// This function adds a new State to the TransitionGraph generating its
-// id incrementally with respects to the previously existent state
-func (fsa *FSA) newState() (id int) {
-	id = len(fsa.states) // Generates a new id
-	fsa.states = append(fsa.states, State{
-		Id:         id,
-		IsFinal:    false,
-		transition: make(map[int][]Transition),
+	// Iterates over the transition in the original FSA, copying them one by one
+	original.ForEachTransition(func(from, to int, t Transition) {
+		localCopy.AddTransition(from, to, t)
 	})
-	return id
+
+	return &localCopy
 }
 
-// This function adds a new Transition and its payload the user can specify the
-// from and to states or eventually can use some special value such as Current
-// for "from" that connect the new state to the latest or newStates for "to"
-// that automatically instantiate a new state as destination of the transition
-// NOTE: If a transition already exist then is overwritten
+// Adds a new Transition to the fsa on which is called, the user can specify to special states
+// respectively Current for "from" that defaults the state from which the transition will start
+// to be the latest created and NewState for "to" that creates a new destination state from scratch
+// for the given transition
 func (fsa *FSA) AddTransition(from, to int, t Transition) {
-	// Input checking
+	// Argument checks
 	if from == Unknown || to == Unknown {
 		log.Fatal("unknown starting or ending state on AddTransition")
 	} else if t.Label == "" {
-		return
+		log.Fatal("empty labels are not allowed")
 	}
 
-	// The user can omit a specific starting state, in this case the
-	// latest added state id is used as starting point of the transition
+	// If the user specified the "Current" flag the starting state used is the latest created
 	if from == Current {
 		from = fsa.currentId
 	}
 
-	// The user can omit the ending state of the new transition, in this
-	// case a new state is created and the transition is linked to that one
+	// If the user specified the "NewState" flag the destination state is created from scratch
 	if to == NewState {
-		to = fsa.newState()
+		to = fsa.GetLastId() + 1
 		fsa.SetRootId(to)
 	}
 
 	// ! Debug print, will be removed later
 	fmt.Printf("BP__ %d -> %d \t %+v\n", from, to, t)
 
-	// Creates/assign the new transition
-	localCopy := fsa.states[from].transition[to]
-	fsa.states[from].transition[to] = append(localCopy, t)
+	// Adds the new transition to the adjacency matrix
+	fsa.transitions[from][to] = append(fsa.transitions[from][to], t)
 }
 
-// TODO COMMENT
+// Returns the id of the state last generated
+func (fsa *FSA) GetLastId() int {
+	return len(fsa.transitions) - 1
+}
+
+// Sets the state identified by the given id as the new root of the FSA, this means that the next
+// transition added with the "Current" flag will start from this node, this is valid until a new
+// state is generated with the NewState flag that overrides this current root state
+func (fsa *FSA) SetRootId(newRootId int) {
+	fsa.currentId = newRootId
+}
+
+// Allows to iterate over each transition currently available in the FSA with a user defined callback
 func (fsa *FSA) ForEachTransition(callback func(from, to int, t Transition)) {
-	for _, fromState := range fsa.states {
-		for destState, transitions := range fromState.transition {
-			for _, t := range transitions {
-				callback(fromState.Id, destState, t)
+	// Iterates over each state in the adjacency matrix
+	for from, outgointTransitions := range fsa.transitions {
+		// Iterates over each outgoing transitions for the abovesaid state
+		for to, parallelTransitions := range outgointTransitions {
+			// Iterates over each parallel transition (with same start and ending state)
+			for _, t := range parallelTransitions {
+				callback(from, to, t)
 			}
 		}
 	}
 }
 
-// This function exports a .png image of the current state of the Graph, it copies state by state
-// and then transition by transition the graph upon which is called, and then saves the GraphViz copy as
-// a .png image file to the provided path
-func (fsa *FSA) ExportAsSVG(imagePath string) {
-	// Creates a GraphViz instance and initializes a Graph instance
-	graphvizInstance := graphviz.New()
-	graphRender, err := graphvizInstance.Graph()
+// Exports the FSA in the format requested, creating/overwriting the path given as argument
+func (fsa *FSA) Export(outputFile string, format graphviz.Format) {
+	// Creates a GraphViz instance and initializes a Graph render object
+	gvInstance := graphviz.New()
+	graph, graphErr := gvInstance.Graph()
 
-	if err != nil {
-		log.Fatal(err)
+	if graphErr != nil {
+		log.Fatal(graphErr)
 	}
 
-	// Initializes a map that will map the TransitionGraph state's id to a cgraph.Node pointer
-	// (a copy of the state that will be rendered). This will be used to render the edges later on
-	associationMap := make(map[int]*cgraph.Node)
-	// Saves the id of the final state
-	finalStateId := fsa.GetFinalStateId()
+	// A simple map to keep track of the cross reference (FSA => graphviz.Graph) between states and nodes
+	state2node := make(map[int]*cgraph.Node)
 
-	// Bulk copy of TransitionGraph.states into renderGraph
-	for _, state := range fsa.states {
-		renderNode, _ := graphRender.CreateNode(fmt.Sprint(state.Id))
-		associationMap[state.Id] = renderNode
-		renderNode.SetShape(cgraph.CircleShape)
-		if finalStateId == state.Id {
-			renderNode.SetShape(cgraph.DoubleCircleShape)
+	// Bulk copy of states from the FSA to the graphviz Graph (as nodes)
+	for stateId := range fsa.transitions {
+		// Creates a cgraph.Node from the current stateId
+		node, nodeErr := graph.CreateNode(fmt.Sprint(stateId))
+		node.SetShape(cgraph.CircleShape) // Default shape
+
+		if nodeErr != nil {
+			log.Fatal(nodeErr)
 		}
-	}
 
-	// Bulk copy of the FSA's Transition into renderGraph
-	for _, state := range fsa.states {
-		for destId, transitions := range state.transition {
-			for i, t := range transitions {
-				from := associationMap[state.Id]
-				to := associationMap[destId]
-				edgeId := fmt.Sprintf("%d-%d-%d", state.Id, destId, i)
-				renderEdge, _ := graphRender.CreateEdge(edgeId, from, to)
-				renderEdge.SetLabel(fmt.Sprint(t))
-			}
+		// If the current state is final state then changes the shapes in the graphical representation
+		if fsa.FinalStates.Contains(stateId) {
+			node.SetShape(cgraph.DoubleCircleShape)
 		}
+
+		// At last updates the association map with the new entries
+		state2node[stateId] = node
 	}
 
-	// Creates a .png export, that saves in current working directory
-	if err := graphvizInstance.RenderFilename(graphRender, graphviz.SVG, imagePath); err != nil {
-		log.Fatal(err)
+	// Bulk copy of transitions from the FSA to the graphviz Graph (as edges)
+	fsa.ForEachTransition(func(from, to int, t Transition) {
+		// Retrieves the references to the graphviz.Graph nodes
+		fromRef, toRef := state2node[from], state2node[to]
+		// Creates a uid for the current edge from the tuple (from, to, t)
+		edgeId := fmt.Sprintf("%d-%d-%s", from, to, t)
+
+		// Creates the edge and sets its label
+		edge, edgeErr := graph.CreateEdge(edgeId, fromRef, toRef)
+		edge.SetLabel(fmt.Sprint(t))
+
+		if edgeErr != nil {
+			log.Fatal(edgeErr)
+		}
+	})
+
+	// Creates an export in the format requested at the given path, there's no enforcing of the fact that
+	// the extension (in the path) and format requested have to match
+	exportErr := gvInstance.RenderFilename(graph, format, outputFile)
+
+	if exportErr != nil {
+		log.Fatal(exportErr)
 	}
 
 	// Cleanup function that closes both the Graph and GraphViz instances
 	defer func() {
-		if err := graphRender.Close(); err != nil {
+		if err := graph.Close(); err != nil {
 			log.Fatal(err)
 		}
-		graphvizInstance.Close()
+		gvInstance.Close()
 	}()
 }
