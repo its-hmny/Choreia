3f6b69dad262d17768122a815ea60497d6790086
Enea Guidi <guidienea@gmail.com>
2022-09-10 00:22:49 +0200

    feat(all): Adds level logging + minor improvements

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/its-hmny/Choreia
 
 go 1.19
 
-require github.com/teris-io/cli v1.0.1
+require (
+	github.com/sirupsen/logrus v1.9.0
+	github.com/teris-io/cli v1.0.1
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- main.go
+++ main.go
@@ -5,11 +5,12 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
-	"strings"
+	"path/filepath"
 
-	"github.com/its-hmny/Choreia/metadata"
+	analysis "github.com/its-hmny/Choreia/go/static_analysis"
+	log "github.com/sirupsen/logrus"
 	"github.com/teris-io/cli"
 )
 
@@ -18,51 +19,45 @@ import (
 const Usage = `Associates a Choreography Automata to your Go program.`
 
 // "metadata" subcommand, extracts the metadata from the given program through static analysis
-var MetadataCmd = cli.
+var metadataCmd = cli.
 	NewCommand("metadata", "Extracts metadata through static analysis").WithShortcut("meta").
 	// Arguments, options and flags registrations
 	WithArg(cli.NewArg("input", "The program entrypoint (main.go)").WithType(cli.TypeString)).
 	WithOption(cli.NewOption("output", "Output file path").WithChar('o').WithType(cli.TypeString)).
-	WithOption(cli.NewOption("verbose", "Verbose logging").WithChar('v').WithType(cli.TypeBool)).
 	// Registers an handler function that will dispatche the argument to the respective module
-	WithAction(MetadataHandler)
+	WithAction(func(args []string, options map[string]string) int {
+		// Destructures the fields from the respective origins
+		input, output := args[0], options["output"]
 
-// Parses and validates arguments coming from the CLI, eventually transforming them or
-// replacing them with default values before calling the wrapped library function.
-func MetadataHandler(args []string, options map[string]string) int {
-	// Destructures the fields from the respective origins
-	input, output, verbose := args[0], options["output"], options["verbose"] == "true"
+		// If no output path is not provided then the file is saved in the same
+		// directory as the input with just a different extension (in this case .json)
+		if output == "" {
+			// Error is ignored since it will be catched later on
+			abspath, _ := filepath.Abs(input)
+			// If no output path is provided then a JSON file with the name of the given
+			// input directory will be saved in the current working directory
+			basename := filepath.Base(abspath)
+			output = fmt.Sprintf("%s.json", basename)
+		}
 
-	if len(args) == 0 {
-		log.Fatal("You must provide a .go input file to be analyzed")
-		return 1
-	}
-	if _, err := os.Stat(input); err != nil {
-		log.Fatal("The path provided doesn't exist, please check it")
-		return 1
-	}
-	if filestat, _ := os.Stat(input); filestat.IsDir() {
-		log.Fatal("Argument must be a file path, directory are not supported yet")
-		return 1
-	}
+		if _, err := analysis.ExtractAndSave(input, output); err != nil {
+			log.Fatal(err)
+		}
+		return 0
+	})
 
-	// If no output path is not provided then the file is saved in the same
-	// directory as the input with just a different extension (in this case .json)
-	if output == "" {
-		output = strings.Replace(input, ".go", ".json", -1)
-	}
-
-	if _, err := metadata.ExtractAndSave(input, output, verbose); err != nil {
-		log.Fatal("Argument must be a file path, directory are not supported yet")
-		return 1
-	}
-
-	return 0
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.TextFormatter{ForceColors: true, FullTimestamp: true, TimestampFormat: "15:04:05"})
+	// Output to stdout instead of the default stderr
+	log.SetOutput(os.Stdout)
+	// Only log the warning severity or above.
+	log.SetLevel(log.TraceLevel)
 }
 
 func main() {
 	// Builds the CLI app with the respective subcommands
-	app := cli.New(Usage).WithCommand(MetadataCmd)
+	app := cli.New(Usage).WithCommand(metadataCmd)
 	// Dispatch the arguments and executes the respective action
 	os.Exit(app.Run(os.Args, os.Stdout))
 }
